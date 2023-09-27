d05ed04863d4cfcfcaae633a7f732e83ffa0d673
RoxanaRincon <roxanarincon98@gmail.com>
2023-09-27 11:54:41 -0500

    ADMIN

--- scriptsactualizacionmapa.sql
+++ scriptsactualizacionmapa.sql
@@ -6,3 +6,19 @@ ALTER TABLE `manzanaservicio`
   ADD COLUMN `latitude` DECIMAL(10, 8) NOT NULL,
   ADD COLUMN `longitude` DECIMAL(11, 8) NOT NULL;
 
+/* SE DEBE ELIMINAR  LA TABLA  ADMIN  Y VOLVERLA A CREAR  ASI PARA IMPLMENTAR PREGUNTAS DE SEGURIDAD */
+
+CREATE TABLE `admin` (
+  `idAdmin` int(11) NOT NULL,
+  `tipoDocumento` varchar(50) NOT NULL,
+  `documento` varchar(50) NOT NULL,
+  `nombres` varchar(50) NOT NULL,
+  `apellidos` varchar(50) NOT NULL,
+  `correoElectronico` varchar(50) NOT NULL,
+  `telefono` varchar(30) NOT NULL,
+  `contraseña` varchar(25) NOT NULL,
+  `pregunta1` varchar(255) DEFAULT NULL,
+  `respuesta1` varchar(255) DEFAULT NULL,
+  `pregunta2` varchar(255) DEFAULT NULL,
+  `respuesta2` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
