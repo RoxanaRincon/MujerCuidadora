075e915e81b68fd30c1731dc86eaa58762371088
RoxanaRincon <roxanarincon98@gmail.com>
2023-09-27 09:04:16 -0500

    ajustes

--- vista/js/mapa.js
+++ vista/js/mapa.js
@@ -1,15 +1,43 @@
-function initMap() {
-    const myLatLng = { lat: -25.363, lng: 131.044 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: myLatLng,
-    });
-  
-    new google.maps.Marker({
-      position: myLatLng,
-      map,
-      title: "Hello World!",
-    });
-  }
-  
-  window.initMap = initMap;
+var map = L.map('map').setView([4.642832837652756, -74.11994579368523], 12);
+
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+
+var greenIcon = L.icon({
+    iconUrl: '../vista/imagenes/iconomanzana.svg',
+    // shadowUrl: 'vista/imagenes/manzana.avif',
+
+    iconSize:     [65, 65], // size of the icon
+    // shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+
+var marker = L.marker([4.590322969137869, -74.09791318650694], {icon: greenIcon}).addTo(map).bindPopup("<b>hola soy una manzana 1!</b><br>I am a popup.");
+
+var marker = L.marker([4.629531954416187, -74.07322412883565], {icon: greenIcon}).addTo(map).bindPopup("<b>hola soy una manzana 2!</b><br>I am a popup.");
+
+var marker = L.marker([4.617838199078608, -74.1239310495157], {icon: greenIcon}).addTo(map).bindPopup("<b>hola soy una manzana 2!</b><br>I am a popup.");
+
+var marker = L.marker([4.755291784099477, -74.11342462883565], {icon: greenIcon}).addTo(map).bindPopup("<b>hola soy una manzana 2!</b><br>I am a popup.");
+
+var marker = L.marker([4.6837717924878834, -74.14169944845952], {icon: greenIcon}).addTo(map).bindPopup("<b>hola soy una manzana 2!</b><br>I am a popup.");
+
+var marker = L.marker([4.635154887984382, -74.05814752408034], {icon: greenIcon}).addTo(map).bindPopup("<b>hola soy una manzana 2!</b><br>I am a popup.");
+
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
--- vista/manzanas.php
+++ vista/manzanas.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SENA - Control de Competencias</title>
-    <!-- Enlaces CDN de Bootstrap, jQuery y Boxicons -->
+     <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+   
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -13,9 +14,17 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-   
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    <link href="https://cdn.datatables.net/v/dt/dt-1.13.6/r-2.5.0/datatables.min.css" rel="stylesheet">
+    <script src="https://cdn.datatables.net/v/dt/dt-1.13.6/r-2.5.0/datatables.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+
+
+    <link rel='stylesheet' type='text/css' media='screen' href='css/mapa.css'>
     <link rel="stylesheet" type="text/css" href="css/navbar.css">
-    <script src="js/mapa.js"></script>
     
 </head>
 <body id="body-pd">
@@ -90,32 +99,44 @@
     </div>
     <!-- Contenido principal -->
     <div class="main-content">
-        <div class="container">
+<div class="container">
+
+            <div id="map">
+
+
+            </div>
+
+        
+        
+
+
         <div class="row">
-        <div class="col-md-4">
-            <h3>Mapa Manzana Antonio Nariño</h3>
-            <img class="scale-with-grid" src="https://manzanasdelcuidado.gov.co/wp-content/uploads/2023/08/map-manz-anton_.svg" alt="Mapa Manzana Antonio Nariño" width="" height="">
+            <div class="col-md-4">
+                <h3>Mapa Manzana Antonio Nariño</h3>
+                <img class="scale-with-grid" src="https://manzanasdelcuidado.gov.co/wp-content/uploads/2023/08/map-manz-anton_.svg" alt="Mapa Manzana Antonio Nariño" width="" height="">
+            </div>
+            <div class="col-md-4">
+                <h3>Manzana Teusaquillo</h3>
+                <img class="scale-with-grid" src="https://manzanasdelcuidado.gov.co/wp-content/uploads/2023/07/map-manz-teu_.svg" alt="Mapa Manzana Teusaquillo" width="" height="">
+            </div>
+            <div class="col-md-4">
+                <h3>Manzana Puente Aranda</h3>
+                <img class="scale-with-grid" src="https://manzanasdelcuidado.gov.co/wp-content/uploads/2023/07/map-manz-pua_.svg" alt="Mapa Manzana Puente Aranda" width="" height="">
+            </div>
+            <div class="col-md-4">
+                <h3>Manzana Suba - Fontanar del Rio</h3>
+                <img class="scale-with-grid" src="https://manzanasdelcuidado.gov.co/wp-content/uploads/2023/07/map-manz-su_.svg" alt="Mapa Manzana Suba - Fontanar del Rio" width="" height="">
+            </div>
+            <div class="col-md-4">
+                <h3>Manzana Fontibón</h3>
+                <img class="scale-with-grid" src="https://manzanasdelcuidado.gov.co/wp-content/uploads/2023/07/map-manz-fo_.svg" alt="Mapa Manzana Fontibón" width="" height="">
+            </div>
+            <div class="col-md-4">
+                <h3>Manzana Chapinero</h3>
+                <img class="scale-with-grid" src="https://manzanasdelcuidado.gov.co/wp-content/uploads/2023/07/map-manz-cha_.svg" alt="Mapa Manzana Chapinero" width="" height="">
+            </div>
         </div>
-        <div class="col-md-4">
-            <h3>Manzana Teusaquillo</h3>
-            <img class="scale-with-grid" src="https://manzanasdelcuidado.gov.co/wp-content/uploads/2023/07/map-manz-teu_.svg" alt="Mapa Manzana Teusaquillo" width="" height="">
-        </div>
-        <div class="col-md-4">
-            <h3>Manzana Puente Aranda</h3>
-            <img class="scale-with-grid" src="https://manzanasdelcuidado.gov.co/wp-content/uploads/2023/07/map-manz-pua_.svg" alt="Mapa Manzana Puente Aranda" width="" height="">
-        </div>
-        <div class="col-md-4">
-            <h3>Manzana Suba - Fontanar del Rio</h3>
-            <img class="scale-with-grid" src="https://manzanasdelcuidado.gov.co/wp-content/uploads/2023/07/map-manz-su_.svg" alt="Mapa Manzana Suba - Fontanar del Rio" width="" height="">
-        </div>
-        <div class="col-md-4">
-            <h3>Manzana Fontibón</h3>
-            <img class="scale-with-grid" src="https://manzanasdelcuidado.gov.co/wp-content/uploads/2023/07/map-manz-fo_.svg" alt="Mapa Manzana Fontibón" width="" height="">
-        </div>
-        <div class="col-md-4">
-            <h3>Manzana Chapinero</h3>
-            <img class="scale-with-grid" src="https://manzanasdelcuidado.gov.co/wp-content/uploads/2023/07/map-manz-cha_.svg" alt="Mapa Manzana Chapinero" width="" height="">
-        </div>
+      
         
     </div>
 </div>
@@ -129,9 +150,9 @@
             © 2023 SENA
         </div>
     </footer>
-    <script src="js/main.js"></script>
+   
 
-    
+    <script src="js/mapa.js"></script>
 </body>
 </html>
 
