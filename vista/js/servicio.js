b927ed202cde24874a77270e3a1c2a62c46bdc8b
Roxana Rincon <117941082+RoxanaRincon@users.noreply.github.com>
2023-09-28 10:11:29 -0500

    cambios servicios.js bontones

--- vista/js/servicio.js
+++ vista/js/servicio.js
@@ -1,10 +1,10 @@
-$(function(){
-
-    tabla = null
+$(function () {
+    tabla = null;
     listarServicio();
     listarCategorias();
     listarTipoServicio();
-    function listarServicio(){
+
+    function listarServicio() {
         var objData = new FormData();
         objData.append("listarServicios", "ok");
         $.ajax({
@@ -19,14 +19,31 @@ $(function(){
             dataSet = [];
             var botonAcciones = '';
             respuesta.forEach(listarServicios);
-            function listarServicios(item,index){
-                botonAcciones = '<button id="btn_editServicio" type="button" class="btn btn-danger" idServicio="'+item.idServicio+'" codigo = "'+item.codigo+'" nombre = "'+item.nombre+'" descripcion = "'+item.descripcion+'" idCategoria = "'+item.idCategoria+'" idTipoServicio = "'+item.idTipoServicio+'"><i  class="bi bi-trash3"></i></button>';
-                botonAcciones += '<button id="btn_eliServicio" type="button" class="btn btn-primary" idServicio="'+item.idServicio+'"><i class="bi bi-pencil-square"></i></button>';
+
+            function listarServicios(item, index) {
+                botonAcciones = '<button class="btn btn-danger btn-eliminar" data-id="' + item.idServicio + '"><i class="bi bi-trash3"></i></button>';
+                botonAcciones += '<button class="btn btn-primary btn-editar" data-id="' + item.idServicio + '" data-codigo="' + item.codigo + '" data-nombre="' + item.nombre + '" data-descripcion="' + item.descripcion + '" data-idCategoria="' + item.idCategoria + '" data-idTipoServicio="' + item.idTipoServicio + '"><i class="bi bi-pencil-square"></i></button>';
                 dataSet.push([item.codigo, item.nombre, item.descripcion, item.idCategoria, item.IdTipoServicio, botonAcciones]);
-                
             }
+
             actualizarTabla(dataSet);
-        })
+
+            // Agregar manejadores de eventos a los botones "Editar" y "Eliminar"
+            $('#tablaServicios').on('click', '.btn-eliminar', function () {
+                var servicioId = $(this).data('id');
+                eliminarServicio(servicioId);
+            });
+
+            $('#tablaServicios').on('click', '.btn-editar', function () {
+                var servicioId = $(this).data('id');
+                var codigo = $(this).data('codigo');
+                var nombre = $(this).data('nombre');
+                var descripcion = $(this).data('descripcion');
+                var idCategoria = $(this).data('idCategoria');
+                var idTipoServicio = $(this).data('idTipoServicio');
+                editarServicio(servicioId, codigo, nombre, descripcion, idCategoria, idTipoServicio);
+            });
+        });
     }
 
     function actualizarTabla(dataSet) {
@@ -36,11 +53,11 @@ $(function(){
 
         tabla = $("#tablaServicios").DataTable({
             data: dataSet
-        })
+        });
         tabla = true;
     }
-    
-    function listarCategorias(){
+
+    function listarCategorias() {
         var objData = new FormData();
         objData.append("listarCategorias", "ok");
         $.ajax({
@@ -54,24 +71,23 @@ $(function(){
         }).done(function (respuesta) {
             opciones = '';
             respuesta.forEach(listarSelectCategorias);
-            function listarSelectCategorias(item,index){
-                opciones += '<option value="' + item.idCategoria + '">' + item.categoria + '</option>';
 
+            function listarSelectCategorias(item, index) {
+                opciones += '<option value="' + item.idCategoria + '">' + item.categoria + '</option>';
             }
-            // console.log(respuesta)
+
             opciones = '';
             respuesta.forEach(listarCategoriasOpciones);
-             // se ajusta nombre
-            function listarCategoriasOpciones(item,index){
+
+            function listarCategoriasOpciones(item, index) {
                 opciones += '<option value="' + item.idCategoria + '">' + item.nombre + '</option>';
             }
 
             $("#sltCategoria").html(opciones);
-        })
+        });
     }
 
-
-    function listarTipoServicio(){
+    function listarTipoServicio() {
         var objData = new FormData();
         objData.append("listarTipoServicio", "ok");
         $.ajax({
@@ -85,27 +101,28 @@ $(function(){
         }).done(function (respuesta) {
             opciones = '';
             respuesta.forEach(listarSelectTipoServicio);
-            function listarSelectTipoServicio(item,index){
-                opciones += '<option value="' + item.idTipoUsuario + '">' + item.nombre + '</option>';
 
+            function listarSelectTipoServicio(item, index) {
+                opciones += '<option value="' + item.idTipoUsuario + '">' + item.nombre + '</option>';
             }
+
             $("#sltTipoServicio").html(opciones);
-        })
+        });
     }
 
-    $("#guardarServicio").on("click", function(){
+    $("#guardarServicio").on("click", function () {
         var codigo = $("#Codigo").val();
-    var nombre = $("#Nombre").val();
-    var descripcion = $("#Descripcion").val();
-    var categoria = $("#sltCategoria").val();
-    var tipoServicio = $("#sltTipoServicio").val();
+        var nombre = $("#Nombre").val();
+        var descripcion = $("#Descripcion").val();
+        var categoria = $("#sltCategoria").val();
+        var tipoServicio = $("#sltTipoServicio").val();
 
-    var objData = new FormData();
-    objData.append("guardarCodigo", codigo);
-    objData.append("guardarNombre", nombre);
-    objData.append("guardarDescripcion", descripcion);
-    objData.append("guardarCategoria", categoria);
-    objData.append("guardarTipoServicio", tipoServicio);
+        var objData = new FormData();
+        objData.append("guardarCodigo", codigo);
+        objData.append("guardarNombre", nombre);
+        objData.append("guardarDescripcion", descripcion);
+        objData.append("guardarCategoria", categoria);
+        objData.append("guardarTipoServicio", tipoServicio);
 
         $.ajax({
             url: "../controlador/servicioControlador.php",
@@ -116,8 +133,52 @@ $(function(){
             contentType: false,
             processData: false
         }).done(function (respuesta) {
-            alert("registro cargado con exito")
-        })
-        
-    })
-})
+            alert("Registro cargado con éxito");
+            $("#servicioModal").modal('hide'); // Cierra el modal después de agregar
+            listarServicio(); // Actualiza la lista después de agregar
+        });
+    });
+
+    function eliminarServicio(servicioId) {
+        if (confirm('¿Estás seguro de que deseas eliminar este servicio?')) {
+            var objData = new FormData();
+            objData.append('eliminarServicio', 'ok');
+            objData.append('idServicio', servicioId);
+
+            $.ajax({
+                url: '../controlador/servicioControlador.php',
+                type: 'post',
+                dataType: 'json',
+                data: objData,
+                cache: false,
+                contentType: false,
+                processData: false
+            }).done(function (respuesta) {
+                if (respuesta.success) {
+                    alert('Servicio eliminado con éxito');
+                    listarServicio(); // Actualiza la lista después de eliminar
+                } else {
+                    alert('Error al eliminar el servicio');
+                }
+            });
+        }
+    }
+
+    function editarServicio(servicioId, codigo, nombre, descripcion, idCategoria, idTipoServicio) {
+        $("#ServicioID").val(servicioId);
+        $("#Codigo").val(codigo);
+        $("#Nombre").val(nombre);
+        $("#Descripcion").val(descripcion);
+        $("#sltCategoria").val(idCategoria);
+        $("#sltTipoServicio").val(idTipoServicio);
+
+        // Abre el modal de edición
+        $("#servicioModal").modal('show');
+
+        // Agregar manejador de evento para cerrar el modal de edición
+        $("#servicioModal").on("hidden.bs.modal", function () {
+            $("#ServicioID").val("0"); // Reinicia el valor del ID del servicio
+            $("#servicioForm")[0].reset(); // Limpia el formulario
+        });
+    }
+});
